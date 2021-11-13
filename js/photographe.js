8827df054484a940fc19270bbc8bae9f4bb6e4b3
Oxana Theis <otheis@protonmail.com>
2021-11-13 16:35:18 +0100

    version avec debut de contact form

--- js/photographe.js
+++ js/photographe.js
@@ -1,6 +1,8 @@
 // recupere les datas depuis.json
 
 const linkToJson = "./FishEyeData.json";
+let tableau_medias = [];
+
 fetch(linkToJson)
   .then(function (response) {
     if (response.ok) {
@@ -32,28 +34,24 @@ fetch(linkToJson)
       //on cree le photographe si son id (de .json) correspond au idTag (id) recuperé dans le lien url
       if (photographe.id == idTag) {
         createPhtotographer(photographe);
-     
-       
+        createTotalLikes(photographe);
         //.................................
-
+        
         for (let media of mediaData) {
-          // console.log(media)
           if (media.photographerId == idTag) {
             // console.log(media.photographerId)
             createMedia(media, photographe);
-         // createLightbox()
-           
+            tableau_medias.push(media);
+            
+            filterPopular(media, photographe);
+            
+         //   console.log(tableau_medias)
+            
           }
-
-         
-          
         }
-        createLightbox();// array cible img/video de photographe 
+        createLightbox(); // array cible img/video de photographe
       }
     }
-
-
-    
   })
 
   .catch(function (err) {
@@ -114,24 +112,91 @@ function createMedia(media, photographe) {
   const mediaContainer = document.querySelector(".galery-photo");
 
   mediaContainer.innerHTML +=
-    
-    '<article>' + choiseMedia(media, photographe) +
-  '<div class="galery-photo-title">'+
-    '<a href="#">'+ '<p>'+ media.title+'</p>'+'</a>'+
-   '<div class="galery-photo-like">'+
-      '<a href="#">'+ '<p>'+ media.likes + '</p>'+ '</a>'+
-      '<a href="#">'+
-        '<p><i class="fas fa-heart" ></i>'+
-          
-          '</p>'+
+    "<article>" +
+    choiseMedia(media, photographe) +
+    `<div class="galery-photo-title">
+    <a href="#">
+  <p> 
+   ${media.title} 
+    </p>
+    </a>
+    <div class="galery-photo-like">
+    <a href="#">
+    <p class = "nb-likes" id = ${media.id}>
+    ${media.likes} 
+    </p>
+    </a>
+    <a href="#">
+    <p class = "heart">`+
 
-    '</a>'+
+   
+
+   `<i class="fas fa-heart"></i>`+
+   
+    '</p>'+
+   ' </a>'+
+   '</div>'+
     '</div>'+
-  '</div>'+
-'</article>';
+    '</article> ';
 
+  //...................................
+  //Rajout de likes pour chaque media
+ // function clickLikes(media) {
+//<i class="fas fa-heart" id = ${media.id} onclick = ${clickLikes(media)}></i>
+ /*
+    const hearts = document.querySelectorAll(".heart i");
+    // la nouvelle variable nbLikes ne recupere pas la valeur de media.likes correctement
+    //recupere derniere valaeur de media
+
+    hearts.forEach((heart) => {
+      heart.addEventListener("click", (e) => {
+  
+        //console.log(heart.id);
+      
+
+        if (heart.id = media.id) {
+          media.likes += 1;
+          document.querySelectorAll(".nb-likes").innerHTML = media.likes;
+          console.log(media.likes)
+       
+      }
+      
+    })
+    });
+  
+ // }
+//}
+//....................
 }
 //......................................................................................
+*/
+
+}
+
+
+function clickLikes(media) {
+
+  
+     const hearts = document.querySelectorAll(".heart i");
+     // la nouvelle variable nbLikes ne recupere pas la valeur de media.likes correctement
+     //recupere derniere valaeur de media
+ 
+     hearts.forEach((heart) => {
+       heart.addEventListener("click", (e) => {
+   
+         //console.log(heart.id);
+       
+ 
+         if (heart.id == media.id) {
+           media.likes += 1;
+           document.querySelectorAll(".nb-likes").innerHTML = media.likes;
+           console.log(media.likes)
+        
+       }
+       
+     })
+     })}
+
 
 function choiseMedia(media, photographe) {
   if (media.image) {
@@ -151,75 +216,148 @@ function choiseMedia(media, photographe) {
     </p>
   </a>`;
   }
-  return
+  return;
 }
 //.......................................................................
 
-function createLightbox(media, photographe){
-const modal = document.querySelector(".modal");
+function createLightbox() {
+  const modal = document.querySelector(".modal");
 
-//const pour cibler img et video de photographe 
-const medias = document.querySelectorAll(".galery-photo__img img, .galery-photo__img video");
-const next = document.querySelector(".lightbox__next .fa-chevron-right");
-const prev = document.querySelector(".lightbox__prev .fa-chevron-left");
-const close = document.querySelector(".lightbox__close .fa-times");
+  //const pour cibler img et video de photographe
+  const medias = document.querySelectorAll(
+    ".galery-photo__img img, .galery-photo__img video"
+  );
+  const next = document.querySelector(".lightbox__next .fa-chevron-right");
+  const prev = document.querySelector(".lightbox__prev .fa-chevron-left");
+  const close = document.querySelector(".lightbox__close .fa-times");
 
+  let lightboxMedia = modal.querySelector(".lightbox__container img");
 
-let lightboxMedia = modal.querySelector(".lightbox__container img")
+  medias.forEach((media, i) => {
+    //console.log(i)
+    media.addEventListener("click", (e) => {
+      //sessionStorage.setItem - stock une paire clé/valeur ici "index", i(number)
+      sessionStorage.setItem("index", i);
+      modal.style.visibility = "visible";
+      lightboxMedia.src = media.src;
+    });
+  });
+  next.addEventListener("click", (e) => {
+    // sessionStorage.getItem - retourne la valeur associée à une clé ici - "index"
+    // on parseInt pour convertir une chaine de caractères en nombre entier
+    let newIndex = parseInt(sessionStorage.getItem("index")) + 1;
+
+    //si on essaye afficher le media suivant suite au dernier media , on cree
+    //une condition pour afficher le premier media
+
+    if (newIndex >= medias.length) {
+      newIndex = 0;
+    }
+
+    //on attribue une nouvelle valeur à la clé "index" , ici  newIndex
+    sessionStorage.setItem("index", newIndex);
+
+    //on attribue la nouvelle src a l'element lightboxMedia = à la nouvelle valeur de newIndex
+    lightboxMedia.src = medias[newIndex].src;
+  });
+
+  prev.addEventListener("click", (e) => {
+    let newIndex = parseInt(sessionStorage.getItem("index")) - 1;
+    //si on n'a pas de media
+    if (newIndex < 0) {
+      // on recepure la longeur totale de tableau medias donc dernier media du tableau
+      newIndex = medias.length;
+    }
+    //on attribue une nouvelle valeur à la clé "index" , ici  newIndex
+    sessionStorage.setItem("index", newIndex);
+    //on attribue la nouvelle src a l'element lightboxMedia = à la nouvelle valeur de newIndex
+    lightboxMedia.src = medias[newIndex].src;
+  });
+
+  close.addEventListener("click", (e) => {
+    modal.style.visibility = "hidden";
+  });
+}
+
+//..........................................................................
+function filterPopular(media) {
+ let medias = [media]
+  const mediaContainer = document.querySelector(".galery-photo");
+
+  //on defini element select de la form
+  const filterSelect = document.querySelector("#listbox");
   
+  //on ecoute le changement des options
+  filterSelect.addEventListener("change", (e) => {
+  //  mediaContainer.innerHTML = "";
+    //console.log(e);
+    //on cree une variable pour recuperer la valeur de l'option choisie
+    let choise = filterSelect.value;
+
+    if (choise == "popularité") {
+      medias.sort((a, b) => (a.likes < b.likes ? 1 : -1));
+      console.log(medias);
+
+      /*
+  if (choise == 'popularite') {
+    medias.sort((a, b) => (a.likes < b.likes ? 1 : -1));
+  } else if (choise == 'date') {
+    console.log(medias);
+    medias.sort((a, b) => (a.date > b.date ? 1 : -1));
+    console.log(medias);
+  } else if (choise == 'titre') {
+    media.sort((a, b) => (a.title > b.title ? 1 : -1));
+  }
+
+*/
+      /*
+   function sortNumber(a,b){
+     
+   return (a.likes)-(b.likes);
+   }
+   media.sort(sortNumber);
+
+   */
+      /*
+ medias.sort(function(a,b){
+   
+   return parseInt(a.likes)-parseInt(b.likes);
+    })
 
 
+*/
 
-medias.forEach((media, i) =>{
-  //console.log(i)
-  media.addEventListener('click', (e)=> {
-    //sessionStorage.setItem - stock une paire clé/valeur ici "index", i(number)
-    sessionStorage.setItem("index", i);
-    modal.style.visibility = "visible";
-    lightboxMedia.src = media.src;
-    
-    
-  })
-})
-next.addEventListener("click",(e)=> {
-// sessionStorage.getItem - retourne la valeur associée à une clé ici - "index"
-// on parseInt pour convertir une chaine de caractères en nombre entier 
-let newIndex = parseInt(sessionStorage.getItem("index"))+1;
-
-
-
-//si on essaye afficher le media suivant suite au dernier media , on cree
-//une condition pour afficher le premier media 
-
-if(newIndex >= medias.length){
-newIndex = 0;
-}
-sessionStorage.setItem("index", newIndex);
-lightboxMedia.src = medias[newIndex].src;
-
-})
-
-
-prev.addEventListener("click",(e)=>{
-
-  let newIndex = parseInt(sessionStorage.getItem("index"))-1;
-//si on n'a pas de media 
-if(newIndex < 0){
-
-// on recepure la longeur totale de tableau medias donc dernier media du tableau 
-newIndex = medias.length;
-}
-sessionStorage.setItem("index", newIndex);
-lightboxMedia.src = medias[newIndex].src;
-
-
-})
-
-close.addEventListener("click",(e)=>{
-
- modal.style.visibility = "hidden";
-})
-
+    }
  
+    
+    else {
+      console.log("bye");
+    }
+  });
 }
 
+function createTotalLikes(data) {
+  const totalLikesContainer = document.querySelector(".total-wrapper");
+
+  totalLikesContainer.innerHTML +=
+    '<div class = "total">' +
+    '<p class="total__likes">' +
+    "297091" +
+    '<i class="fas fa-heart">' +
+    "</i>" +
+    "</p>" +
+    '<p class="total__price">' +
+    data.price +
+    "€ / jour" +
+    "</p>" +
+    "</div>" +
+    "</div>";
+}
+
+const contactBtn = document.querySelector("#contact")
+console.log(contactBtn)
+const formModal = document.querySelector(".form-modal")
+
+contactBtn.addEventListener("click", (e) => {
+formModal.style.visibility= "visible";
+})
